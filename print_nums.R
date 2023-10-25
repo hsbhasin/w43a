a43a37f679d25fee68395e9a0e867aed8166a8cd
hsbhasin <146058964+hsbhasin@users.noreply.github.com>
2023-10-25 12:04:47 +0800

    Add files via upload

---
+++ print_nums.R
@@ -0,0 +1,18 @@
+#Mthd 1a - For Loop Priniting Int from 1- 10
+for (number in 1:10) print(number)
+
+# Mthd 1b - For Loop
+for (number in 1:10){
+  print(number)
+}
+
+#Mthd 2 - While Loop
+number <- 1
+while(number <= 10){
+  print(number)
+  number <- number + 1
+}
+
+#Method 3 
+print(1:10)
+cat(1:10)
---
+++ print_nums_python.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Method 1 - For Loop
+"""For Loop"""
+for count in range (1,11):
+    print (count)
+    
+#Method 2 - while loop
+i = 1
+while (i<= 10):
+    print(i)
+    i = i + 1
+    
+# Method 3
+print(1,2,3,4,5,6,7,8,9,10)    
